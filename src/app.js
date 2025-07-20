263437deeca4eac1005c351f83b3b5a98f60b7c7
Britney Escoffery <britneybescoffery@gmail.com>
2025-07-20 23:03:28 +0000

    added functions to generate domain names using JS

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ['their', 'her', 'my', 'his'];
+let adjective = ['pink', 'sparkly', 'small', 'cool'];
+let noun = ['lantern', 'glacier', 'melody', 'compass'];
+let extension = ['com', 'io', 'biz', 'store'];
+
+let domainNamesArray = [];
+let domainCk, extLen; 
+
+for (i = 0; i < pronoun.length; i++) {
+    for (j = 0; j < adjective.length; j++) {
+        for (k = 0; k < noun.length; k++) {
+            for (l = 0; l < extension.length; l++) {
+            }
+        }
+    }
+}
+
+    
+
+extLen = extension[l].length;
+extLen = noun[k].slice(-1*extLen);
+
+if(domainCk === extension[l]){
+    domainNamesArray.push(pronoun[i]+adjective[j]+noun[k].slice(0,(-1))*extLen)+'.'+extension[l];
+    domainNamesArray.push(pronoun[i]+adjective[j]+noun[k]+'.'+extension[l]);
+}
+else{
+    domainNamesArray.push(pronoun[i]+adjective[j]+noun[k]+extension[l]);
+}
+
+const domainNamesArrayShuffled = domainNamesArray.sort((x, y) => 0.5 - Math.random());
+document.getElementById("list-of-domains").innerHTML = domainNamesArrayShuffled.join("<br>");
