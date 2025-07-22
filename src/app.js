752ba368affd68531824ab61836a0a172c43ddc5
Britney Escoffery <britneybescoffery@gmail.com>
2025-07-22 01:34:22 +0000

    added javascript loops to html

--- src/app.js
+++ src/app.js
@@ -3,17 +3,3 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-
-function generateDomains() {
-  let domainList = '';
-  for (let p of pronoun) {
-    for (let a of adjective) {
-      for (let n of noun) {
-        for (let ext of extension) {
-          domainList += `<div>${p}${a}${n}.${ext}</div>`;
-        }
-      }
-    }
-  }
-  document.getElementById('domains').innerHTML = domainList;
-}
